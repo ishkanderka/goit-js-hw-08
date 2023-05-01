ac27a42f2e22b288af28543f37250b1f00437dde
Victoria Shmidt <reachme@victoriashmidt.com>
2023-05-01 14:28:49 -0600

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(data.seconds)
+    );
+  }, 1000)
+);
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
